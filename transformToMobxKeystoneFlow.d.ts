fcded0b59d4747f6335916d5d322a74c60846bd4
Amit Tsur <amit.tsur@gmail.com>
2023-09-19 11:48:56 +0300

    Added autoModel doc

--- transformToMobxKeystoneFlow.d.ts
+++ transformToMobxKeystoneFlow.d.ts
@@ -45,3 +45,23 @@ class Test {
 ```
  */
 declare function autoFlow(target: Object, propertyKey: string | symbol): void;
+
+/** 
+ * Marks an model class to add a @model decorator to it with the relative path as the value 
+ * by [ts-transform-async-to-mobx-keystone-flow](https://github.com/Tsury/ts-transform-async-to-mobx-keystone-flow) 
+ * @example
+```
+// in:
+@autoModel
+class Test {
+}
+
+// out:
+import { model as model_1 } from 'mobx-keystone';
+
+@model_1("relative/path/filename")
+class Test {
+}
+```
+ */
+declare const autoModel: ClassDecorator;
